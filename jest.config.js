5ea61b42e0302889892882e63d1308ab1f0612b4
gabyzam <ruth.gabriela.pz@gmail.com>
2023-02-18 00:31:33 -0500

    Añadí cambios a la configuración de jest y package

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
     coverageDirectory: "coverage",
     testEnvironment: "jsdom",
     setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+    "transform": {
+        "^.+\\.(js|jsx)$": "babel-jest"
+    }
 }
