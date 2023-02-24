d39ee1e5b1c897b989f5f944cce08de075f0ecd7
gabyzam <ruth.gabriela.pz@gmail.com>
2023-02-23 23:14:10 -0500

    Añadí propiedad type a la función que realiza la lógica de petición a la API

--- src/api/searchProducts.js
+++ src/api/searchProducts.js
@@ -1,17 +1,19 @@
 import { API_URL } from './API';
 import axios from 'axios';
 
-export const searchProducts = async (token, value, limit, page) => {
+export const searchProducts = async (token, value, limit, page, type) => {
   try {
     const config = {
       headers: { Authorization: `Bearer ${token}` },
     };
     const products = await axios.get(
-      `${API_URL}/products/search?search=${value}&page=${page}&limit=${limit || 12}`,
+      `${API_URL}/products/${type}?${type}=${value}&page=${page}&limit=${limit || 12}`,
       config
     );
+    console.log(products);
     return products.data;
   } catch (error) {
     console.log(error.response.data.error);
+    return false;
   }
 };
