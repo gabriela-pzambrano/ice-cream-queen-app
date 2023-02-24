e173c56d808de6f4e2bec4fc6d1afa12fac0fe13
gabyzam <ruth.gabriela.pz@gmail.com>
2023-02-23 22:42:48 -0500

    Mofifiqué archivo App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import ProtectedRoute from './pages/ProtectedRoute';
+
 function App() {
   return (
     <main className="bg-background h-full overflow-hidden">
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/" element={<ProtectedRoute />}>
+          <Route index element={<Dashboard />} />
+        </Route>
       </Routes>
+
       <ToastContainer
         position="top-center"
         autoClose={2500}
